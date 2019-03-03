8ee74e6bd0de5ac9a3dfd2d4b9491d72483c1f86
Cary <carymeskell@gmail.com>
2019-03-02 16:27:56 -0800

    add rspec watch (bundle exec guard) and add unit tests for #has_name?

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem "rspec"
+gem 'guard'
+gem 'guard-shell'
+gem 'rb-fsevent', '~> 0.9'
---
+++ spec/item_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe 'Item' do
+
+  context "#has_name?" do
+    item = Item.new('machete', 'It\'s a sharp blade', {aliases: ['rusty machete', 'macheetah']})
+
+    it "should return true if provided name matches item\'s name" do
+      name = 'machete'
+
+      expect(item.has_name?(name)).to eql(true)
+    end
+
+    it "should return true if the provided name matches one of item\'s aliases" do
+      name = 'rusty machete'
+
+      expect(item.has_name?(name)).to eql(true)
+    end
+
+    it "should return false if the provided name does not match item\'s name or aliases" do
+      name = 'machachacha'
+
+      expect(item.has_name?(name)).to eql(false)
+    end
+  end
+end

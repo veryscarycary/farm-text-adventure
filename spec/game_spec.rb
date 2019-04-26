ab2f8db5d9892a281851d88b9917f14a96192902
Cary <carymeskell@gmail.com>
2019-04-26 01:08:49 -0700

    add unit test for take_item

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
 describe 'Game' do
+  mockLocation = nil
   mockPlayer = nil
   mockMap = nil
   game = nil
 
   before(:each) do
-    mockPlayer = instance_double('Player')
-    mockMap = instance_double('Map')
+    mockLocation = instance_double('Location', :remove_item => nil)
+    mockMap = instance_double('Map', :current_location => mockLocation)
+    mockPlayer = instance_double('Player', :add_to_inventory => nil)
     game = Game.new(mockPlayer, mockMap)
   end
 
@@ -23,5 +25,41 @@ describe 'Game' do
         expect(game.map).to eql(mockMap)
       end
     end
+  end
+
+  context "#take_item" do
+      map = nil
+      player = nil
+
+      before(:each) do
+        map = game.instance_variable_get(:@map)
+        player = game.instance_variable_get(:@player)
+
+        allow(map).to receive(:current_location) { mockLocation }
+        allow(player).to receive(:add_to_inventory)
+      end
+
+      after(:each) do
+        Game.any_instance.unstub(:_check_for_item)
+      end
+
+      it "should put it into player's inventory" do
+        item = Item.new('rock', '', { applicable_commands: [:take, :drop] })
+        Game.any_instance.stub(:_check_for_item).and_return(item)
+
+        game.take_item(item.name)
+
+        expect(player).to have_received(:add_to_inventory).with(item)
+      end
+
+      it "should remove item from location if the item doesn't belong to another item" do
+        item = Item.new('rock', '', { applicable_commands: [:take, :drop] })
+        Game.any_instance.stub(:_check_for_item).and_return(item)
+
+        game.take_item(item.name)
+
+        expect(map).to have_received(:current_location)
+        expect(mockLocation).to have_received(:remove_item).with(item)
+      end
   end
 end
